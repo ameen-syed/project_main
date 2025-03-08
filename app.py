d3c4b5dc7455a6f987d7bc83e36bdf10f698bd08
ameen-syed <130234090+ameen-syed@users.noreply.github.com>
2025-03-08 21:35:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
         while True:
             dist = distance()
             print ("Measured Distance = %.1f cm" % dist)
-            if(dist < 5):
+            if(dist < 10):
                 GPIO.output(23,GPIO.HIGH)
                 print ("Danger")
             else:
