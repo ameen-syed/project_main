0d343a2b9a4f66551c27e5d3adabea36b32f8b6f
ameen-syed <130234090+ameen-syed@users.noreply.github.com>
2025-04-05 09:59:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,12 +51,12 @@ if __name__ == '__main__':
             print ("Measured Distance = %.1f cm" % dist)
             if(dist < 10):
                 GPIO.output(23,GPIO.HIGH)
-                GPIO.output(2,GPIO.HIGH)
+                GPIO.output(2,GPIO.LOW)
                 print ("Danger")
             else:
                 #print ("LED off")
                 GPIO.output(23,GPIO.LOW)
-                GPIO.output(2,GPIO.LOW)
+                GPIO.output(2,GPIO.HIGH)
                 print ("Safe")
             time.sleep(1)
  
